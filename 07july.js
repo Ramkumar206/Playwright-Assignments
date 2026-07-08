b1f8cbd617033e76b34e00e366357c35d9575233
Ramkumar206 <RAM.MANO91@GMAIL.COM>
2026-07-08 11:21:09 +0530

    Add files via upload
    Week1Day2

---
+++ 07july.js
@@ -0,0 +1,24 @@
+let firstName = "Ramkumar"
+console.log( firstName )
+console.log(typeof firstName)
+let companyName="Accenture"
+console.log(companyName);
+console.log(typeof companyName);
+let mobileNumber=8787787887
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+let isAutomation=false
+console.log(isAutomation);
+console.log(typeof isAutomation);
+let hasPlaywright=
+console.log();
+console.log(typeof hasPlaywright);
+
+
+
+
+
+
+
+
+
